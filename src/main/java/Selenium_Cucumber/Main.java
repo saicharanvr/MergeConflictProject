e1b465d496ffeba947da91205f86063a9c81da17
Saicharan Reddy <saicharanvr99@gmail.com>
2025-04-20 18:23:03 +0530

    Update Main.java

--- src/main/java/Selenium_Cucumber/Main.java
+++ src/main/java/Selenium_Cucumber/Main.java
@@ -3,7 +3,7 @@ package Selenium_Cucumber;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+        System.out.println("Hello world!!!!!");
     }
     public static void doSomething(String[] args) {
 
