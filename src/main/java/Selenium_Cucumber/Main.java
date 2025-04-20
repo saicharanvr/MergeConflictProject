a698c41bde701613e6df03761b5eb1ca0fdb6081
saicharanvr <saicharanvr99#gmail.com>
2025-04-20 19:11:05 +0530

    f2 commit 1

--- src/main/java/Selenium_Cucumber/Main.java
+++ src/main/java/Selenium_Cucumber/Main.java
@@ -3,8 +3,14 @@ package Selenium_Cucumber;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+        System.out.println("Hello world!!!!!");
     }
+    public static void doSomething(String[] args) {
 
+        System.out.println("Charan Sai");
+        System.out.println("complete change");
+        System.out.println("complete 1");
+        System.out.println("complete 1");
+    }
 
 }
